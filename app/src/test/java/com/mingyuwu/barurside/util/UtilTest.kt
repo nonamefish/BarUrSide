67a210e0d5c907cdd96c3b9a48ae71783d35e7f3
MingYu-Wu <annie82920@gmail.com>
2021-11-28 09:51:55 +0800

    unit testing: test get diff hours and days

--- app/src/test/java/com/mingyuwu/barurside/util/UtilTest.kt
+++ app/src/test/java/com/mingyuwu/barurside/util/UtilTest.kt
@@ -1,13 +1,12 @@
 package com.mingyuwu.barurside.util
 
-import com.mingyuwu.barurside.R
+import com.mingyuwu.barurside.util.Util.getDiffDay
+import com.mingyuwu.barurside.util.Util.getDiffHour
 import com.mingyuwu.barurside.util.Util.getDiffMinute
 import junit.framework.TestCase
 import org.junit.Test
 import java.sql.Timestamp
-import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 class UtilTest : TestCase() {
 
@@ -20,7 +19,6 @@ class UtilTest : TestCase() {
 
         // When
         val diff = getDiffMinute(Timestamp(cdate.timeInMillis))
-        println("date: ${Timestamp(cdate.timeInMillis)}")
 
         // Then
         assertEquals(diff, 5)
@@ -28,10 +26,30 @@ class UtilTest : TestCase() {
 
     @Test
     fun testGetDiffHour() {
+        // Given
+        val cdate = Calendar.getInstance()
+        cdate.time = Timestamp(System.currentTimeMillis())
+        cdate.add(Calendar.HOUR, -5)
+
+        // When
+        val diff = getDiffHour(Timestamp(cdate.timeInMillis))
+
+        // Then
+        assertEquals(diff, 5)
     }
 
     @Test
     fun testGetDiffDay() {
+        // Given
+        val cdate = Calendar.getInstance()
+        cdate.time = Timestamp(System.currentTimeMillis())
+        cdate.add(Calendar.DATE, -5)
+
+        // When
+        val diff = getDiffDay(Timestamp(cdate.timeInMillis))
+
+        // Then
+        assertEquals(diff, 5)
     }
 
     @Test
