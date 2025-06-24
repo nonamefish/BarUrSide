1de4ebbb59cb4ee0ac072a9df5205eff7220f666
MingWu <annie82920@gmail.com>
2025-06-24 16:11:31 +0800

    refactor: remove item_rating_bitmap databinding logic

--- app/src/main/java/com/mingyuwu/barurside/BindingAdapters.kt
+++ app/src/main/java/com/mingyuwu/barurside/BindingAdapters.kt
@@ -1,63 +1,11 @@
 package com.mingyuwu.barurside
 
-import android.graphics.Bitmap
-import android.os.Build
-import android.text.format.DateFormat
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.annotation.RequiresApi
-import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.text.HtmlCompat
-import androidx.core.text.HtmlCompat.fromHtml
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.mingyuwu.barurside.data.Drink
-import com.mingyuwu.barurside.data.RatingInfo
-import com.mingyuwu.barurside.data.TagFriend
-import com.mingyuwu.barurside.rating.*
 import com.mingyuwu.barurside.util.CurrentFragmentType
-import com.mingyuwu.barurside.util.Style
-import com.mingyuwu.barurside.util.Util
-import com.mingyuwu.barurside.util.Util.getDiffDay
-import com.mingyuwu.barurside.util.Util.getDiffHour
-import com.mingyuwu.barurside.util.Util.getDiffMinute
-import com.mingyuwu.barurside.venue.MenuAdapter
-import java.sql.Timestamp
-import java.time.LocalTime
-import java.util.*
-import kotlin.math.roundToInt
 
-
-@BindingAdapter("imageUrl")
-fun bindImage(imgView: ImageView, imgUrl: String?) {
-    if (!imgUrl.isNullOrEmpty()) {
-        Glide.with(imgView.context)
-            .load(imgUrl)
-            .placeholder(R.drawable.image_placeholder)
-            .error(R.drawable.image_placeholder)
-            .into(imgView)
-    } else {
-        imgView.setBackgroundResource(R.drawable.image_placeholder)
-    }
-}
-
-@BindingAdapter("imgHeight", "imgWidth")
-fun bindImgSize(imageView: ImageView, imgHeight: Int, imgWidth: Int) {
-    val dpToPx = BarUrSideApplication.appContext!!.resources.displayMetrics.density
-    imageView.layoutParams.height = imgHeight * dpToPx.toInt()
-    imageView.layoutParams.width = imgWidth * dpToPx.toInt()
-}
-
-@BindingAdapter("imageBitmap")
-fun bindRecyclerViewWithImageBitmap(imageView: ImageView, imageBitmap: Bitmap?) {
-    imageBitmap?.let {
-        imageView.setImageBitmap(imageBitmap)
-    }
-}
 
 @BindingAdapter("iconNotify")
 fun bindIconNotify(constraintLayout: ConstraintLayout, currentFragmentType: CurrentFragmentType?) {
